d517e51ce8ea90352a4ad11413880cbebc880e63
Sudhakar <pingtosudhakar@gmail.com>
2024-03-05 14:07:02 +0800

    Initial commit

---
+++ basic-service/src/main/java/com/sudhakar/basicservice/BasicServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sudhakar.basicservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicServiceApplication.class, args);
+	}
+
+}
---
+++ basic-service/src/test/java/com/sudhakar/basicservice/BasicServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudhakar.basicservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
